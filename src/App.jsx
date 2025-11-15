77db18ea21194cc439051a52dcb70e63f66251fc
Devanshkulsh <devkulsh1308@gmail.com>
2025-11-15 14:07:32 +0530

    feat: Remove DigitalHerbalGardenTable component and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import PdfViewerPage from "./components/PdfViewerPage";
-import DigitalHerbalGardenTable from "./pages/DigitalHerbalGardenTable";
 import DigitalHerbalGardenFinal from "./Pages/DigitalHerbalGardenFinal";
 
 function App() {
   return (
     <Routes>
       <Route path="/pdf/:name" element={<PdfViewerPage />} />
-      <Route path="garden-table" element={<DigitalHerbalGardenTable />} />
       <Route path="/" element={<DigitalHerbalGardenFinal />} />
     </Routes>
   );
